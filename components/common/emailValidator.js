f21e1065d7d4ac8248167eb8db699754eb505193
Ragil Prasetya <ragil@smartsparrow.com>
2013-02-21 21:30:32 +1100

    [AELP-1988] Removed simcapi components from git tree

--- components/common/emailValidator.js
+++
@@ -1,8 +0,0 @@
-define([], function() {
-  var regexp = /^(([^<>()\[\]\\.,;:\s@\"]+(\.[^<>()\[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  var validateEmail = function(email) {
-    return regexp.test(email);
-  };
-
-  return validateEmail;
-});
