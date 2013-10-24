2913b67c1da9c8307924497f382806da0ff109a9
Elias <elias@smartsparrow.com>
2013-10-24 15:09:53 +1100

    [re-write] - Forgot to remove the removed test from the test.js

--- test/specs/test.js
+++ test/specs/test.js
@@ -5,8 +5,5 @@ define([
   'specs/api/snapshot/SimCapi.spec.js',
   'specs/api/snapshot/SimCapiValue.spec.js',
   'specs/api/snapshot/SimCapiHandler.spec.js',
-  'specs/api/snapshot/SnapshotSegment.spec.js',
-
-  // Util
-  'specs/util/Extender.spec.js'
+  'specs/api/snapshot/SnapshotSegment.spec.js'
 ]);
