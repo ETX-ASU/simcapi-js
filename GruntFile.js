4876b052e6f09926b493dcb8768fe5e6c63e8cbc
Ragil Prasetya <ragil@smartsparrow.com>
2014-05-15 00:22:23 +1000

    updated to 0.55

--- GruntFile.js
+++ GruntFile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: '<json:package.json>',
-    version: '0.54',
+    version: '0.55',
 
     // Clean
     clean: {
