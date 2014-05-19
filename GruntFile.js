b2438563a517ae5d5daa613f1626628eabdb62ad
Ragil Prasetya <ragil@smartsparrow.com>
2014-05-19 13:57:18 +1000

    bump version

--- GruntFile.js
+++ GruntFile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: '<json:package.json>',
-    version: '0.55',
+    version: '0.56',
 
     // Clean
     clean: {
